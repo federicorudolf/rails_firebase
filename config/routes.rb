dfa41a66e0229970547f2e9c421131809602b7bb
Fede Rudolf <federico@launchpadlab.com>
2023-01-02 15:12:04 -0300

    Added views and bugfixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/', to: 'pages#index'
+  get '/signup', to: 'pages#signup'
+  get '/home', to: 'pages#home'
+  
+  post '/login', to: 'pages#login'
+  post '/logout', to: 'pages#logout'
+  post '/signup', to: 'pages#signup'
+  
+  # post '/create_todo', to 'pages#create_todo'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
